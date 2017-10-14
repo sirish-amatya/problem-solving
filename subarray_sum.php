d627bf4e1186e7191d1bd0eca5ffcbe7ca9631af
Sirish <sirish.amatya@gmail.com>
2017-10-14 18:40:41 +1100

    Create a function to find the sum of subarray

---
+++ subarray_sum.php
@@ -0,0 +1,37 @@
+<?php
+
+/*
+For a given array $arr, finds the first contiguous subarray whose sum matches $sum
+If found, prints Found along with indexes else prints Not found. 
+*/
+function subarray_sum($arr, $sum)
+{
+    $len = count($arr);
+    $curr_sum = $arr[0];
+    $found = false;
+    $j = 0;
+    for ($i=1; $i<$len; $i++) {
+
+        $curr_sum = $curr_sum+$arr[$i];
+  
+        while ($curr_sum>$sum && $j<$i) {
+        
+            $curr_sum = $curr_sum-$arr[$j];
+            $j++;
+        }
+
+        if ($curr_sum==$sum) {
+        
+            $found = true;
+            $found_pos = "{$j}, {$i}";
+            break;
+        } 
+
+    }
+    return ($found)?$found_pos:-1;
+
+}
+
+print subarray_sum([1,2,3,4,5,7], 23);
+
+?>
